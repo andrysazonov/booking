d2ae0641a4277c2c7167336600c06cf7ce99af58
andrysazonov <andrysazonov@gmail.com>
2021-12-03 00:12:10 +0500

    выбор даты и времени. заготовка столов

--- ClientApp/src/components/Map/index.js
+++ ClientApp/src/components/Map/index.js
@@ -1,8 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./map.module.css";
 export const Map = () => {
-    return (
-<div>карта</div>
-        
-    )
-}
+  const getTable = (count) => {
+    let template = [];
+    for (let i = 0; i <= count; i++) {
+      template.push(
+        <div key={i} className={styles.table}>
+          Стол {i + 1}
+        </div>
+      );
+    }
+    return template;
+  };
+  return (
+    <div className={styles.map}>
+      <div>{getTable(5)}</div>
+    </div>
+  );
+};
--- ClientApp/src/pages/BookingPage/index.js
+++ ClientApp/src/pages/BookingPage/index.js
@@ -1,24 +1,52 @@
-import React from 'react'
-import { Map } from '../../components/Map'
-import styles from "./booking.module.css"
+import React, { useState } from "react";
+import { Map } from "../../components/Map";
+import styles from "./booking.module.css";
+import DatePicker, { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import ru from "date-fns/locale/ru";
+registerLocale("ru", ru);
 
 export const BookingPage = () => {
-    return (
-        <div className={styles.bookingContainer}>
-            <div className={styles.controlPanel}>s</div>
-            <div className={styles.bookingSide}>
-                <div className={styles.map}>
-                    <Map/>
-                </div>
-            </div>
-            
-
-
-
-
-
-
-
+  const [date, setDate] = useState(new Date());
+  const [startTime, setStartTime] = useState();
+  return (
+    <div className={styles.bookingContainer}>
+      <div className={styles.controlPanel}>
+        Выберите дату
+        <DatePicker
+          dateFormat="dd.MM.yy"
+          locale={"ru"}
+          selected={date}
+          onChange={(date) => setDate(date)}
+        />
+        Выберите время
+        <DatePicker
+          selected={date}
+          onChange={(date) => setDate(date)}
+          showTimeSelect
+          showTimeSelectOnly
+          timeIntervals={15}
+          timeCaption="Time"
+          dateFormat="HH:mm"
+        />
+        До скольки вы хотите забронировать
+        <DatePicker
+          selected={date}
+          onChange={(date) => setDate(date)}
+          showTimeSelect
+          showTimeSelectOnly
+          timeIntervals={15}
+          timeCaption="Time"
+          dateFormat="HH:mm"
+        />
+        <button className={styles.button}>Показать свободные столы</button>
+        <button className={styles.buttonSubmit}>Заброинровать</button>
+      </div>
+      <div className={styles.bookingSide}>
+        <div className={styles.map}>
+          <Map />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
